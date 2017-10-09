6261b1e39a73d279fbfa56f094f19acfb3fe0e4b
kevin lee <kl2695@nyu.edu>
2017-10-09 13:30:49 -0700

    Add custom errors.

--- error_handling/super_useful.rb
+++ error_handling/super_useful.rb
@@ -1,6 +1,11 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+
+  begin
+    Integer(str)
+  rescue ArgumentError
+    nil
+  end
 end
 
 # PHASE 3
@@ -9,24 +14,35 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  else
+    raise StandardError
+  end
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
+  begin
   puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
 
+  maybe_fruit = gets.chomp
+  reaction(maybe_fruit)
+rescue
+  puts "Not a fruit! Try again"
+    retry
+  end
+end
+class FriendError < StandardError
+
+end
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise FriendError.new, "Must have a name!" if name.empty?
     @name = name
+    raise FriendError.new, "Real friends must have known each other for at least 5 years!" if yrs_known < 5
     @yrs_known = yrs_known
+    raise FriendError.new, "Must have a favorite pastime!" if fav_pastime.empty?
     @fav_pastime = fav_pastime
   end
 
@@ -39,8 +55,6 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
-
-
